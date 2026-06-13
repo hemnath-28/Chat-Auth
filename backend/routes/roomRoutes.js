22a687047347056901d26908827089e1227a28c1
hemnath-28 <hemnath0628@gmail.com>
2026-06-13 13:59:03 +0530

    Added get Messages/Members Routes

--- backend/routes/roomRoutes.js
+++ backend/routes/roomRoutes.js
@@ -1,11 +1,12 @@
 const express=require('express')
 const RoomRoutes=express.Router()
-const {createRoom,joinRoom}=require("../controllers/roomController")
+const {createRoom,joinRoom,getRoomMessage,getroomMembers}=require("../controllers/roomController")
 const jwtverify=require("../middleware/VerifyToken")
 
 RoomRoutes.post("/create",jwtverify,createRoom)
 RoomRoutes.post("/join/:id",jwtverify,joinRoom)
-
+RoomRoutes.get("/:joinid/message",jwtverify,getRoomMessage)
+RoomRoutes.get("/:joinid/members",jwtverify,getroomMembers)
 
 module.exports=RoomRoutes
 
