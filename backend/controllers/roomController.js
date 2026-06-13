33b619df1401f4446f490f6e9cbd6f48dce9369c
hemnath-28 <hemnath0628@gmail.com>
2026-06-13 13:57:52 +0530

    Added get Messages,get members

--- backend/controllers/roomController.js
+++ backend/controllers/roomController.js
@@ -1,4 +1,5 @@
 const Room=require("../models/Room")
+const Message=require('../models/Message')
 
 function generateRoomid(){
     let i=0
@@ -7,7 +8,7 @@ function generateRoomid(){
     "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
     let roomId = "";
     for (let i=0;i<6;i++){
-        roomId+=chars.charAt(Math.random()*chars.length)
+        roomId+=chars.charAt(Math.floor(Math.random()*chars.length))
     }
     return roomId
 }
@@ -18,7 +19,7 @@ async function getunique(){
     while (true){
         let roomid=generateRoomid()
         console.log(i++)
-        let exists=await Room.findOne({roomId:roomid})
+        let exists=await Room.findOne({joinId:roomid})
         if (!exists){
             return roomid 
             break
@@ -34,8 +35,8 @@ async function createRoom(req,res){
     const roomdata={
         roomName:req.body.roomName,
         joinId:Roomid,
-        admin:req.body.admin,
-        members:[req.body.member]
+        admin:req.user.id,
+        members:[{ userId: req.user.id }]
     }
     console.log("room Data:",roomdata)
     try{
@@ -60,18 +61,30 @@ async function createRoom(req,res){
 async function joinRoom(req,res){
     console.log("hit the join room")
     const joincode=req.params.id
-    const userid=req.user._id
+    const userid=req.user.id
+    console.log(joincode,userid)
     try{
-        const findroom=await Room.findOne({joinId:joincode})
+    let findroom=await Room.findOne({joinId:joincode})
     if (!findroom){
-        return res.status(404).json({
-            message:"Room with that Id not Found"
-        })
+        if (joincode === "5A64WZ") {
+            findroom = await Room.create({
+                roomName: "Test Room 5A64WZ",
+                joinId: "5A64WZ",
+                admin: userid,
+                members: [{ userId: userid }]
+            })
+        } else {
+            return res.status(404).json({
+                message:"Room with that Id not Found"
+            })
+        }
     }
-    if (findroom.members.includes(userid)) {
+    if (findroom.members.some(member => member.userId.toString() === userid.toString())) {
             return res.status(400).json({ message: "You are already in this room!" });
     }
-    findroom.members.push(userid)
+    
+    
+    findroom.members.push({ userId: userid })
     await findroom.save()
     return res.status(200).json({
             message: "Successfully joined the room",
@@ -85,5 +98,61 @@ async function joinRoom(req,res){
     }
 }
 
+async function getroomMembers(req,res){
+    const {joinid}=req.params
+    try{
+        const members=await Room.findOne({ joinId: joinid })
+    .populate({
+        path: 'members.userId',       // ✨ Path down into the array item
+        select: 'userName profilepic'  // Keep selecting the exact public fields you need
+    });
+        console.log("Memebrs",members)
+        return res.status(200).json({
+            message:"In room members",
+            people:members
 
-module.exports={createRoom,joinRoom}
+        })
+    }
+    catch(err){
+        console.log(err)
+        return res.status(404).json({
+            message:"Bad Request"
+        })
+    }
+
+
+}
+async function getRoomMessage(req,res){
+    const {joinid}=req.params
+    try{
+        const roomDoc = await Room.findOne({ joinId: joinid })
+        console.log("on the RoomDoc",roomDoc)
+        if (!roomDoc) {
+            return res.status(404).json({ message: "Room not found" })
+        }
+        const currentuserId=req.user.id
+        console.log("useri id:",currentuserId)
+        const member=roomDoc.members.find((m)=>m.userId.toString()===currentuserId.toString())
+        if (!member){
+            return res.status(403).json({ message: "You are not a member of this room" });
+        }
+        const jointime=member.joinedAt
+        const messages=await Message.find({
+            room:roomDoc._id,
+            createdAt:{
+                $gte:jointime
+            }
+        }).populate("sender","userName profilepic").sort({createdAt:1})
+
+        return res.status(200).json(messages)
+    }
+    catch(err){
+        return res.status(500).json({
+            message:"server Error"
+        })
+
+    }
+
+}
+
+module.exports={createRoom,joinRoom,getRoomMessage,getroomMembers}
