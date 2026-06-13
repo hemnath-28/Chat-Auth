ed609f76ee1fc49a42ae4ea2cc6b8187db2b3289
hemnath-28 <hemnath0628@gmail.com>
2026-06-13 19:49:22 +0530

    main File server.js

---
+++ backend/server.js
@@ -0,0 +1,101 @@
+const express=require('express')
+const cors = require('cors');
+const passport=require('passport')
+require('dotenv').config()
+const Room=require("./models/Room")
+const connectDb=require("./database/connectDb")
+const http=require('http')
+const { Server }=require("socket.io")
+const authRoutes=require("./routes/authRoutes")
+const userRoutes=require("./routes/userRoutes")
+const roomRoutes=require("./routes/roomRoutes")
+const messageRoutes=require("./routes/messageRoutes")
+const Message=require("./models/Message")
+const app=express()
+app.use(cors())
+app.use(express.json())
+app.use(passport.initialize())
+
+const onlineUsers=new Map();
+app.set('onlineUsers', onlineUsers);
+const connect=connectDb()
+const Port=process.env.PORT
+const server=http.createServer(app)
+
+const io=new Server(server,{
+    cors:{
+        origin: "http://localhost:5173", // Your React frontend port
+        methods: ["GET", "POST","DELETE","PUT"]
+    }
+})
+
+io.on("connection",(socket)=>{
+    let globalroomid=null
+    console.log("A user is Connected via web Socket",socket.id)
+    // on User Connected attach id to it and emit to everyone user-connected
+    socket.on("user_connected",(userid)=>{
+        socket.userid=userid
+        onlineUsers.set(socket.userid,socket.id)
+        if (globalroomid){
+            socket.broadcast.to(globalroomid).emit("alert", {
+        message: "New user has been Joined"
+    });
+
+        }
+        io.emit("user-joined",{
+            userId:userid,status:"online"
+        })
+    })
+    socket.on("join_room",(roomid)=>{
+        globalroomid=roomid
+        socket.join(roomid)
+        console.log(`Socket ${socket.id} joined room room: ${roomid}`);
+    })
+    socket.on("send_message",async (msg)=>{
+        const { joinid, sender, content } = msg;
+        console.log("message:",msg)
+        const roomDoc = await Room.findOne({ joinId: joinid });
+        if (!roomDoc) {
+            console.log("Room not found for joinId:", joinid);
+            return;
+        }
+        const newMessage = await Message.create({
+            room: roomDoc._id,     // Room ObjectID
+            sender: sender, // User ObjectID
+            content: content
+        });
+        console.log("message Saved")
+        const populatemessage=await newMessage.populate("sender", "userName profilepic")
+        io.emit("receive_message",populatemessage)
+    })
+    // On user disconnect remove from map and emit user-left
+    socket.on("disconnect",()=>{
+        onlineUsers.delete(socket.userid)
+        io.emit("user-left",{
+            userId:socket.userid,status:"offline"
+        })
+        socket.broadcast.to(globalroomid).emit("alert",{
+            message:"User has been left"
+        })
+    })
+})
+
+
+
+
+
+app.use("/message",messageRoutes)
+
+app.use("/auth",authRoutes)
+
+app.get("/",(req,res)=>{
+    res.send("In the Home page")
+})
+
+app.use("/user",userRoutes)
+
+app.use("/room",roomRoutes)
+server.listen(Port,()=>{
+    console.log(`Server is Listening on Port ${Port}`)
+})
+
